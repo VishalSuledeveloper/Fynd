ea1e15637572ae9c8ae560013d312787b411ca40
Vishal <vishalsule2017@gmail.com>
2023-02-04 23:29:13 +0530

    third commit

---
+++ NODEJS/quotient.js
@@ -0,0 +1,10 @@
+var math= require('mathematics'); 
+var x=4;
+var y=2;
+var z=x/y;
+console.log("value of x is ;",x);
+console.log("value of y is ;",y);
+console.log("For finding quotient of any two number we just do x/y.\n So, We get :")
+console.log("Quotient is :",z);
+
+// console.log(Math.pow(5,1));
