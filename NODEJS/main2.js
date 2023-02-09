82db4b8604f2427ce0415664d70c6590f355eaf9
Vishal <vishalsule2017@gmail.com>
2023-02-09 20:45:17 +0530

    commit four

--- NODEJS/main2.js
+++ NODEJS/main2.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var dt =require('./ownModule.js');
+var m2= require('./main1.js');
 
 http.createServer(function(req,res){
     
@@ -8,14 +9,14 @@ http.createServer(function(req,res){
     //Head: code indicating success or failure or any other status
     //200:success code,format: text/html,json.xml....
     res.writeHead(200,{'Context-Type':'text/html'});
+    
 
     //update the body of the response: data coming from a different user created module
     res.write("The data and time are currently : " +dt.getCurrentDate());
     // res.write("\nTime are currently :" +dt.getCurrentTime());
     //date time is being fetched from the outside module
     //using the content type mentioned above as text/html user defined module
-    res.write("\nThis program is running on port number 8000");
+    res.write("\nProgram running on port number 8002");
 
     res.end();//ending the write functionality for a request response function
-}).listen(8000);//mentioning which port our application will run on the server
-
+}).listen(8002);//mentioning which port our application will run on the server
